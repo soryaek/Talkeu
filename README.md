f799e5bad8d268d4fc83567fe26a35428230751c
soryaek <sorya.ek@gmail.com>
2024-08-17 21:38:55 -0700

    add local project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Talkeu
+
+<h3>How to run in local environment: </h3>
+<ul>
+  <li>Install packages: <b>npm install</b> </li>
+  <li>Start running:  <b>npm start</b></li>
+  <li>Open your web browser and then run: <b>http://localhost:3000/ </b></li>
+</ul>
+
+<img src="public/images/talkeu-welcome-page.png" width="800px" alt="welcome page"/>
+
+<img src="public/images/talkeu-chat.png" width="800px" alt="chat page" />
---
+++ server.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const http = require('http');  
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const chatBot = 'Talkeu Chatbot';
+
+
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    socket.emit('message', formatMessage(chatBot, 'Welcome to Talkeu!'));
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(chatBot, `${user.username} has joined the chat`)
+      );
+
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(chatBot, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
